61a4688bb8b56399c8a4f49cca21761b591029f7
Mehran Shahidi <m.mehrashahidi@gmail.com>
2022-01-13 16:20:54 +0330

    feat: nvm support & some aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,13 +6,13 @@ set PATH $PATH /home/mehran/.cargo/bin
 set PATH $PATH /home/mehran/.yarn/bin
 abbr -a ws 'sudo systemctl start windscribe'
 abbr -a wind 'windscribe connect'
+set TERMINAL (which nvim)
 set EDITOR (which nvim) 
-set -g theme_color_scheme dracula
 #-----------------------------------------------------
 #Aliases
 #-------------------------------------------------------
-#alias vim="nvim"
-alias lf lfrun
+alias nn="nvim"
+# alias lf lfrun
 alias color "colorscript random"
 alias ll "exa --icons -l -g"
 alias tlmgr "/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode"
@@ -58,38 +58,3 @@ set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
 
   
-# TokyoNight Color Palette
-# set -l foreground E1E3E4
-# set -l selection 364A82
-# set -l comment 565f89
-# set -l red f7768e
-# set -l orange ff9e64
-# set -l yellow e0af68
-# set -l green 9ece6a
-# set -l purple 9d7cd8
-# set -l cyan 7dcfff
-# set -l pink bb9af7
-# 
-# # Syntax Highlighting Colors
-# set -g fish_color_normal $foreground
-# set -g fish_color_command $cyan
-# set -g fish_color_keyword $pink
-# set -g fish_color_quote $yellow
-# set -g fish_color_redirection $foreground
-# set -g fish_color_end $orange
-# set -g fish_color_error $red
-# set -g fish_color_param $purple
-# set -g fish_color_comment $comment
-# set -g fish_color_selection --background=$selection
-# set -g fish_color_search_match --background=$selection
-# set -g fish_color_operator $green
-# set -g fish_color_escape $pink
-# set -g fish_color_autosuggestion $comment
-# 
-# # Completion Pager Colors
-# set -g fish_pager_color_progress $comment
-# set -g fish_pager_color_prefix $cyan
-# set -g fish_pager_color_completion $foreground
-# set -g fish_pager_color_description $comment
-
-
