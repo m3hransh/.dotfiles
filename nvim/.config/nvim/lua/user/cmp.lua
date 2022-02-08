8cdf756f672516dfe9be139a1af01c842b988b02
Mehran Shahidi <m.mehrashahidi@gmail.com>
2022-02-08 11:17:49 +0330

    ✨ Extend friendly_snippets with custom snippets

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -8,7 +8,9 @@ if not snip_status_ok then
 	return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
+local friendly_snippets = '~/.local/share/nvim/site/pack/packer/start/friendly-snippets'
+local mysnippets = './my-snippets' --overriding friendly snippets
+require("luasnip/loaders/from_vscode").lazy_load({paths= {mysnippets, friendly_snippets}})
 
 local check_backspace = function()
 	local col = vim.fn.col(".") - 1
